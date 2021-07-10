74f49e8f8540e8372b3593ee5bd20afa4e43b892
superbottlegod <superbottlegod@naver.com>
2021-07-10 20:35:34 +0900

    7/10

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.db.migrations.operations import models
+from . import models
 
 # Register your models here.
+@admin.register(models.User)
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
