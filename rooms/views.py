1dc2d1f1f4dd408715dcc01ef2d57660c5d12d2a
superbottlegod <superbottlegod@naver.com>
2021-07-25 15:52:37 +0900

    7/25

--- rooms/views.py
+++ rooms/views.py
@@ -54,11 +54,51 @@ class RoomDetail(DetailView):
 
 
 def search(request):
-    city = (request.GET.get("city") or "Anywhere")
+    city = request.GET.get("city") or "Anywhere"
     city = str.capitalize(city)
-    print(city)
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+    instant = request.GET.get("instant", False)
+    super_host = request.GET.get("super_host", False)
+    # 여러개를 선택한 걸 가져올 땐 getlist!
+    selected_amenities = request.GET.getlist("amenities")
+    selected_facilities = request.GET.getlist("facilities")
+
+    form = {
+        "city": city,
+        "selected_room_type": room_type,
+        "selected_country": country,
+        "price": price,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
+        "selected_amenities": selected_amenities,
+        "selected_facilities": selected_facilities,
+        "instant": instant,
+        "super_host": super_host,
+    }
+
     room_types = models.RoomType.objects.all()
-    return render(request, "rooms/search.html", {"city": city, "countries": countries, "room_types": room_types})
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
+    }
+
+    return render(
+        request,
+        "rooms/search.html",
+        {**form, **choices},
+    )
 
 
 # Function Based View
