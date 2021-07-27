978eabb1ba5a3c18f108bbe68eef241ae189df39
superbottlegod <superbottlegod@naver.com>
2021-07-28 01:51:17 +0900

    7/27

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
+from django.views import View
 from django.shortcuts import render
 
 # Create your views here.
+# CBV
+class LoginView(View):
+
+    def get(self, request):
+        return render(request, "users/login.html")
+
+    def post(self, request):
+        pass
+
+#FBV
+def login_view(request):
+    if request.method == "GET":
+        pass
+    elif request.method == "POST":
+        pass
