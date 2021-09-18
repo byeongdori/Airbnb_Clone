ea4fc49144dce128047ba2e428a886f1059fbb08
superbottlegod <superbottlegod@naver.com>
2021-09-18 17:45:00 +0900

    9/18

--- reservations/urls.py
+++ reservations/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
         name="create",
     ),
     path("<int:pk>", views.ReservationDetailView.as_view(), name="detail"),
+    path(
+        "<int:pk>/<str:verb>", views.edit_reservation, name="edit"
+    ),
 ]
