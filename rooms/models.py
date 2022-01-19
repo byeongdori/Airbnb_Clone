98ed0836878e2dccf75e04de9225d162b037f6d5
superbottlegod <superbottlegod@naver.com>
2022-01-20 00:22:22 +0900

    1/19

--- rooms/models.py
+++ rooms/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 from django_countries.fields import CountryField
-from requests.models import ReadTimeoutError
 from core import models as core_models
 from cal import Calendar, calendar
 
